b27faa39460618beb6babd20303c631dab656668
anikett01 <113915085+anikett01@users.noreply.github.com>
2022-09-20 14:24:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class DefaultConfig:
 
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "cf8addba-0bb4-4bd4-aa41-7e7508f926bf") #Enter your Microsoft App Id here (Azure Bot App ID)
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "0a4524fe-e29b-49aa-95b6-4ee0e4428b6d") #Enter your Microsoft App Password here
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "wXY8Q~tYaFX4M0kICOs8F3o1WNE.Jj9M4TntzcE2") #Enter your Microsoft App Password here
     #EXPIRE_AFTER_SECONDS = os.environ.get("ExpireAfterSeconds", 60)
