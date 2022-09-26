afb878e40b7c7f628cd3f6f619489dade6bd22a2
anikett01 <113915085+anikett01@users.noreply.github.com>
2022-09-26 18:02:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ class DefaultConfig:
     APP_ID = os.environ.get("MicrosoftAppId", "27683de7-f005-409e-85ca-8aeea2db0b98") #Enter your Microsoft App Id here (Azure Bot App ID)
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "ywH8Q~OIvL_61ZdZy1RZ5nkapbNiNGaDA9WxwaY4") #Enter your Microsoft App Password here
     #EXPIRE_AFTER_SECONDS = os.environ.get("ExpireAfterSeconds", 60)
+
+    
